e35f80ccd4d6686b33bc36dd0999a34e341d7282
octav <150651279+Faaaanw@users.noreply.github.com>
2024-06-23 18:47:23 +0700

    Update application.py

--- app/application.py
+++ app/application.py
@@ -29,7 +29,7 @@ def download_video():
             yd.download(download_dir)
             result_label.config(text=f"Video berhasil disimpan di: {download_dir}")
 
-            # Menampilkan notifikasi setelah video diunduh
+            
             show_notification(f"Video '{yt.title}' berhasil diunduh!")
 
         except Exception as e:
@@ -38,11 +38,11 @@ def download_video():
     video_url = url_entry.get()
     download_dir = "./Downloads"
 
-    # Membuat jendela progres
+    
     progress_window = tk.Toplevel(window)
     progress_window.title("Progress")
 
-    # Menambahkan logo YouTube ke dalam jendela progres
+    
     image_path = os.path.join(os.getcwd(), "youtube_logo.png")
     youtube_logo = Image.open(image_path)
     youtube_logo = youtube_logo.resize((200, 200), )
@@ -60,23 +60,23 @@ def download_video():
     progress_bar.pack(pady=10)
     progress_bar.start()
 
-    # Menjalankan proses pengunduhan pada thread terpisah
+    
     download_thread = threading.Thread(target=download)
     download_thread.start()
 
-    # Memanggil fungsi untuk menutup jendela progres setelah selesai diunduh
+    
     window.after(100, check_download_complete, download_thread, progress_window)
 
 def check_download_complete(download_thread, progress_window):
-    # Menutup jendela progres jika proses pengunduhan telah selesai
+   
     if not download_thread.is_alive():
         progress_window.destroy()
 
-# Membuat instance dari kelas Tk (jendela utama)
+
 window = tk.Tk()
 window.title("YouTube Downloader")
 
-# Menambahkan logo YouTube di atas kata "YouTube Downloader"
+
 image_path = os.path.join(os.getcwd(), "youtube_logo.png")
 youtube_logo = Image.open(image_path)
 youtube_logo = youtube_logo.resize((100,100), resample=Image.ANTIALIAS if hasattr(Image, 'ANTIALIAS') else Image.BICUBIC)
@@ -87,7 +87,7 @@ logo_label = tk.Label(window, image=youtube_logo, text="YouTube Downloader", com
 logo_label.image = youtube_logo
 logo_label.pack(pady=20)
 
-# Membuat label, entry, dan button
+
 url_label = Label(window, text="Masukkan link video:")
 url_label.pack(pady=10)
 
@@ -106,8 +106,8 @@ views_label.pack(pady=5)
 result_label = Label(window, text="")
 result_label.pack(pady=10)
 
-# Menetapkan ukuran awal layar
+
 window.geometry("700x700")
 
-# Memulai loop utama
+
 window.mainloop()
